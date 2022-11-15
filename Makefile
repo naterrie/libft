4bee53dfb9075d1788c64ba3ceb83ca4cd51d74c
Nathan Terrier <naterrie@z2r1p3.42lyon.fr>
2022-11-15 19:50:38 +0100

    putstrfdWork

--- Makefile
+++ Makefile
@@ -34,7 +34,7 @@ SRCS	=	ft_isalpha.c \
 			ft_strmapi.c \
 			ft_striteri.c \
 			ft_putchar_fd.c \
-
+			ft_putstr_fd.c \
 
 CC	= cc
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: naterrie <naterrie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 23:42:23 by naterrie          #+#    #+#             */
-/*   Updated: 2022/11/15 19:37:47 by naterrie         ###   ########lyon.fr   */
+/*   Updated: 2022/11/15 19:40:48 by naterrie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ void	*memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 
 void	ft_bzero(void *s, size_t n);
+void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 
