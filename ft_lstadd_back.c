04203122e30a6cfad8f88676ee3c4e38b03355bc
Nathan Terrier <naterrie@z1r12p5.42lyon.fr>
2022-11-17 17:46:16 +0100

    lstadd back work

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: naterrie <naterrie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 16:49:03 by naterrie          #+#    #+#             */
-/*   Updated: 2022/11/17 17:07:24 by naterrie         ###   ########lyon.fr   */
+/*   Updated: 2022/11/17 17:44:59 by naterrie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	if (lst && *lst)
+	if (*lst)
 	{
 		last = ft_lstlast(*lst);
 		last->next = new;
 	}
-	if (lst && !*lst)
+	else
 		*lst = new;
 }
