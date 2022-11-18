d5e429778ba6867c2d030f5258b9d6dd1b583c64
Nathan Terrier <naterrie@z2r1p2.42lyon.fr>
2022-11-18 14:46:02 +0100

    lstmap work, end

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: naterrie <naterrie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 21:18:41 by naterrie          #+#    #+#             */
-/*   Updated: 2022/11/17 21:37:47 by naterrie         ###   ########lyon.fr   */
+/*   Updated: 2022/11/18 14:45:34 by naterrie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,21 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*temp;
+	t_list	*templst;
 
+	temp = NULL;
 	if (!lst || !f || !del)
 		return (NULL);
-	return (0);
+	while (lst)
+	{
+		templst = ft_lstnew(f(lst->content));
+		if (!templst)
+		{
+			ft_lstclear(&temp, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&temp, templst);
+		lst = lst->next;
+	}
+	return (temp);
 }
